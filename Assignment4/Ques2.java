21983dc2443020c089c60ab9ba42fbc6005f07d3
Anish43 <70018561+Anish43@users.noreply.github.com>
2020-10-12 07:34:39 +0530

    Add files via upload

---
+++ Assignment4/Ques2.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+import java.lang.Integer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Integer initial = -1;
+        Scanner sc = new Scanner(System.in);
+        byte bytedNum = num.byteValue();
+        char charNum = (char)bytedNum;
+        int finalInt = charNum;
+        System.out.println(finalInt);
+    }
+}
